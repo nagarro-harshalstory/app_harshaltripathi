d1f740c1233e867ec5f6e8154a9c92ebab90f8ba
Harshal Tripathi <harshal.free@gmail.com>
2022-02-23 21:36:53 -0500

    Updating gitignore and setting up basic spring boot application

---
+++ src/test/java/com/nagarro/devops/test/AppHarshaltripathiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.devops.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppHarshaltripathiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
